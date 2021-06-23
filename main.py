b9a3e06a1a0c23ae4bc470e7e084732f8ee08d6a
skytomo <tomo1kawa.fullerene@gmail.com>
2021-06-23 21:40:16 +0900

    Add python command

--- main.py
+++ main.py
@@ -53,6 +53,21 @@ async def neko(ctx, text=''):
         await ctx.send(''.join(text).replace('な', 'にゃ') + 'にゃ', allowed_mentions=None)
 
 
+@slash_client.slash(name="python",
+                    description='任意のPythonコードを実行します。',
+                    options=[
+                        create_option(
+                            name="command",
+                            description="実行する命令",
+                            option_type=3,
+                            required=True
+                        )
+                    ])
+async def python(ctx, command: str):
+    if (len(command) - len(command.replace('_', '')) > 10):
+        await ctx.send(f"そのコードはセキュリティ上の問題で実行しにゃいにゃ！")
+    else:
+        await ctx.send(f"```python\n>>> {command}\n{eval(command, {'__builtins__': None})}```")
 
 
 @slash_client.slash(name="nick",
